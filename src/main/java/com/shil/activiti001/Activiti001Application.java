8c278efab9617f70ce6e6097ae1f1a077b506c94
shiliang <1191275239@qq.com>
2020-01-17 20:05:28 +0800

    init..

---
+++ src/main/java/com/shil/activiti001/Activiti001Application.java
@@ -0,0 +1,13 @@
+package com.shil.activiti001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activiti001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Activiti001Application.class, args);
+	}
+
+}
