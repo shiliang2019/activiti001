395188f98e1de5ac9bbe5e730f7f582dfee7b62a
shiliang <1191275239@qq.com>
2020-01-18 18:36:03 +0800

    test activiti6

--- src/main/java/com/shil/activiti001/controller/TestController.java
+++ src/main/java/com/shil/activiti001/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.shil.activiti001.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
 
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.impl.persistence.entity.UserEntityImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +22,9 @@ public class TestController {
 	@Autowired
 	private DataSource dataSource;
 	
+	@Autowired
+	private IdentityService identityService;
+	
 	@RequestMapping(value="/")
 	@ResponseBody
 	public String test()
@@ -27,5 +33,17 @@ public class TestController {
 		System.out.println("-----");
 		System.out.println(dataSource);
 		return "分享牛";
+	}
+	
+	@RequestMapping(value="/user")
+	@ResponseBody
+	public UserEntityImpl user(HttpServletRequest request)
+	{
+		String id = request.getParameter("id");
+		UserEntityImpl user = new UserEntityImpl();
+		user.setId(id);
+		user.setRevision(0);
+		identityService.saveUser(user);
+		return user;
 	}
 }
